7553852f86001a368f3d6707dd6a9311d853f4dc
MKGaru <dev@igaru.com>
2023-02-05 11:25:39 +0800

    update docs

--- system/sleep/typings/sleep.d.ts
+++ system/sleep/typings/sleep.d.ts
@@ -1,5 +1,5 @@
 declare module "spice/system/sleep" {
-	enum WakeupCause {
+	export enum WakeupCause {
 		unknown = 0,
 		WakeupExt0 = 2,
 		WakeupExt1 = 3,
@@ -15,7 +15,17 @@ declare module "spice/system/sleep" {
 		/** sleep time */
 		static light(ms: number): void
 
+		/**
+		 * @see https://lastminuteengineers.com/esp32-pinout-reference/#esp32-rtc-gpio-pins for ESP32
+		 * @param pin rtc-gpio pin
+		 * @param level 
+		 */
 		static enableExt0Wakeup(pin: number, level: 0|1): void
+		/**
+		 * @see https://lastminuteengineers.com/esp32-pinout-reference/#esp32-rtc-gpio-pins for ESP32
+		 * @param pin rtc-gpio pin
+		 * @param level 
+		 */
 		static enableExt1Wakeup(pin: number, level: 0|1): void
 
 		static getWakeupCause(): WakeupCause
